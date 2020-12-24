1215aad4f975f9725a5270335d59ebaa0cd34110
Ethan Miller <ethanzanemiller@gmail.com>
2020-12-24 13:29:54 -0800

    Add dependencies and a few changes to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject tablecloth.time "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Time series manipulation library built on top of tablecloth"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :license {:name "The MIT Licence"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [scicloj/tablecloth "5.00-beta-21"]
+                 [scicloj/notespace "3-alpha3-SNAPSHOT"]
+                 [aerial.hanami "0.12.4"]]
   :repl-options {:init-ns tablecloth.time})
