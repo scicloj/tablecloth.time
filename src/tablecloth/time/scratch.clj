84b40c8e028a18753b6fb355b2d004fa39aa1471
Ethan Miller <ethanzanemiller@gmail.com>
2020-12-26 17:05:44 -0800

    Restructure dirs to match namespace

--- src/tablecloth/time/scratch.clj
+++ src/tablecloth/time/scratch.clj
@@ -1,6 +1,8 @@
 (ns tablecloth.time.scratch
   (:require [tablecloth.api :as tablecloth]
             [tablecloth.time.index :refer [index-by slice]]
+            [notespace.api :as notespace]
+            [notespace.kinds :as kind]
             [clojure.pprint :refer [pprint]]))
 
 (def path "./data/air-passengers.csv")
@@ -16,7 +18,14 @@
 
 (meta data)
 
+(meta (index-by data :Month))
+
 (-> data
     (index-by :Month)
     (slice "1949-01-01" "1949-07-01"))
+
+;; Resample?
+(-> data
+    (tablecloth/add-or-replace-column :Year (fn [ds] (map #(.getYear %) (:Month ds))))
+    (index-by :Year))
 
