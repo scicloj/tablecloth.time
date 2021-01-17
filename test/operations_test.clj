24bef36a8e302647658f2ff9adeb885b4a34fcc2
Ethan Miller <ethanzanemiller@gmail.com>
2021-01-17 11:41:22 -0800

    Restructure tests

--- test/operations_test.clj
+++ test/operations_test.clj
@@ -17,26 +17,25 @@
                     (partition 2 (interleave (columns dsa) (columns dsb))))]
     (and colnames-equal cols-equal)))
 
-(deftest slice-by-date
-  (is (ds-equal? (dataset {:A [(t/date "1970-01-09") (t/date "1970-01-10")]
-                           :B [8 9]})
-                 (-> (dataset {:A (plus-temporal-amount (t/date "1970-01-01") (range 10) :days)
-                               :B (range 10)})
-                     (index-by :A)
-                     (ops/slice-by-date "1970-01-09" "1970-01-10")))))
-
-(deftest slice-by-year
-  (is (ds-equal? (dataset {:A [(t/year "1979") (t/year "1980")]
-                           :B [9 10]})
-                 (-> (dataset {:A (plus-temporal-amount (t/year 1970) (range 11) :years)
-                               :B (range 11)})
-                     (index-by :A)
-                     (ops/slice-by-year "1979" "1980")))))
-
-(deftest slice-by-datetime
+(deftest slice
+  ;; local-date-time
   (is (ds-equal? (dataset {:A [(t/date-time "1970-01-01T09:00") (t/date-time "1970-01-01T10:00")]
                            :B [9 10]})
                  (-> (dataset {:A (plus-temporal-amount (t/date-time "1970-01-01T00:00") (range 11) :hours)
                                :B (range 11)})
                      (index-by :A)
-                     (ops/slice-by-datetime "1970-01-01T09:00" "1979-01-01T10:00")))))
+                     (ops/slice "1970-01-01T09:00" "1979-01-01T10:00"))))
+  ;; year
+  (is (ds-equal? (dataset {:A [(t/year "1979") (t/year "1980")]
+                           :B [9 10]})
+                 (-> (dataset {:A (plus-temporal-amount (t/year 1970) (range 11) :years)
+                               :B (range 11)})
+                     (index-by :A)
+                     (ops/slice "1979" "1980"))))
+  ;; local-date
+  (is (ds-equal? (dataset {:A [(t/date "1970-01-09") (t/date "1970-01-10")]
+                           :B [8 9]})
+                 (-> (dataset {:A (plus-temporal-amount (t/date "1970-01-01") (range 10) :days)
+                               :B (range 10)})
+                     (index-by :A)
+                     (ops/slice "1970-01-09" "1970-01-10")))))
