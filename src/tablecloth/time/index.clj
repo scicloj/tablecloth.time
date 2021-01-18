f0e154cc7627a28ff4af9b9dc2269f2beec1f4b5
Ethan Miller <ethanzanemiller@gmail.com>
2021-01-18 10:04:22 -0800

    Add type hints to prevent reflection (#3)

--- src/tablecloth/time/index.clj
+++ src/tablecloth/time/index.clj
@@ -2,15 +2,17 @@
   (:import java.util.TreeMap)
   (:require [tablecloth.api :as tablecloth]))
 
+(set! *warn-on-reflection* true)
+
 (defn make-index
   "Returns an index for `dataset` based on the specified `index-column-key`."
   [dataset index-column-key]
-  (-> dataset
-      (tablecloth/rows :as-maps)
-      (->> (map-indexed (fn [row-number row-map]
-                          [(index-column-key row-map) row-number]))
-           (into {})
-           (TreeMap.))))
+  (let [row-maps (-> dataset (tablecloth/rows :as-maps))
+        idx-map (->> row-maps
+                     (map-indexed (fn [row-number row-map]
+                                    [(index-column-key row-map) row-number]))
+                     (into {}))]
+    (TreeMap. ^java.util.Map idx-map)))
 
 (defn index-by
   "Returns a dataset with an index attached as metadata."
