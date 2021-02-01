e83ffa1aca9792e1ca350d7c8d02ddc0595ba575
Ethan Miller <ethanzanemiller@gmail.com>
2021-01-31 17:54:16 -0800

    Save rough version of modified slice that can return indeces

--- src/tablecloth/time/index.clj
+++ src/tablecloth/time/index.clj
@@ -22,12 +22,18 @@
                      (into {}))]
     (TreeMap. ^java.util.Map idx-map)))
 
-(defn slice-index [dataset from to]
-  (let [^TreeMap index (get-index-meta dataset)
-        row-numbers (if (not index)
-                      (throw (Exception. "Dataset has no index specified."))
-                      (-> index (.subMap from true to true) (.values)))]
-    (tablecloth/select-rows dataset row-numbers)))
+(defn slice-index
+  ([dataset from to] (slice-index dataset from to nil))
+  ([dataset from to {:keys [result-type]
+                     :or {result-type :as-dataset}
+                     :as options}]
+   (let [^TreeMap index (get-index-meta dataset)
+         row-numbers (if (not index)
+                       (throw (Exception. "Dataset has no index specified."))
+                       (-> index (.subMap from true to true) (.values)))]
+     (condp = result-type
+       :as-indexes row-numbers
+       (tablecloth/select-rows dataset row-numbers)))))
 
 (defn index-by
   "Returns a dataset with an index attached as metadata."
