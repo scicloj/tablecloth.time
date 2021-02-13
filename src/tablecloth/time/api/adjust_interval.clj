e8cbd72cfa6fc710c1943523d6ed02939e3ed0f8
Ethan Miller <ethanzanemiller@gmail.com>
2021-02-12 17:04:45 -0800

    Add support for ungrouping and instant target

--- src/tablecloth/time/api/adjust_interval.clj
+++ src/tablecloth/time/api/adjust_interval.clj
@@ -4,76 +4,60 @@
             [tablecloth.api :as tablecloth]
             [tick.alpha.api :as tick]))
 
-
 (def map-time-unit->time-converter
   {:quarter (fn [datetime] (YearQuarter/from datetime))
    :year-month tick/year-month
-   :year tick/year})
+   :year tick/year
+   :instant tick/instant})
 
 (defn adjust-interval
   "Change the time index frequency."
-  [dataset index-col-key keys target-unit]
-  (let [time-converter (target-unit map-time-unit->time-converter)
-        index-column  (index-col-key dataset)
-        adjusted-column-data (emap time-converter target-unit (index-col-key dataset))]
-    (println {:time-converter time-converter
-              :index-column index-column
-              :adjusted-column-data (take 10 adjusted-column-data)})
-    (-> dataset
-        (tablecloth/add-or-replace-column target-unit adjusted-column-data)
-        (tablecloth/group-by (into [target-unit] keys)))))
+  ([dataset index-col-key keys target-unit]
+   (adjust-interval dataset index-col-key keys target-unit nil))
+  ([dataset index-col-key keys target-unit {:keys [ungroup?]
+                                            :or {ungroup? false}}]
+   (let [time-converter (target-unit map-time-unit->time-converter)
+         index-column  (index-col-key dataset)
+         adjusted-column-data (emap time-converter target-unit (index-col-key dataset))]
+     (-> dataset
+         (tablecloth/add-or-replace-column target-unit adjusted-column-data)
+         (tablecloth/group-by (into [target-unit] keys))
+         (cond-> ungroup? tablecloth/ungroup
+                 (not ungroup?) identity)))))
 
 (comment
   (def raw-ds
     (-> "https://raw.githubusercontent.com/techascent/tech.ml.dataset/master/test/data/stocks.csv"
         (tablecloth/dataset {:key-fn keyword})))
 
+  raw-ds
+
   (def ds-msft
     (-> raw-ds
         (tablecloth/select-rows #(= "MSFT" (:symbol %)))))
 
   (tablecloth/head ds-msft)
-;; => https://raw.githubusercontent.com/techascent/tech.ml.dataset/master/test/data/stocks.csv [5 3]:
-;;    | :symbol |      :date | :price |
-;;    |---------|------------|--------|
-;;    |    MSFT | 2000-01-01 |  39.81 |
-;;    |    MSFT | 2000-02-01 |  36.35 |
-;;    |    MSFT | 2000-03-01 |  43.22 |
-;;    |    MSFT | 2000-04-01 |  28.37 |
-;;    |    MSFT | 2000-05-01 |  25.45 |(.get #time/date "2020-12-10" java.time.temporal.IsoFields/QUARTER_YEAR)
 
-  ;; doing some things more manually
-  (-> ds-msft
-      (tablecloth/add-or-replace-column :year-month (fn [ds] (map tick/year-month (:date ds))))
-      (tablecloth/group-by [:year-month :symbol]))
 
-  ;; what if the index column is less granular? will that ever happen? I don't think that makes sense. Can't image a use-case.
-
-  ;; works
-  (-> raw-ds
-      (adjust-interval :date [:symbol] :year-month)
-      (tablecloth/aggregate {:price #(tech.v3.datatype.functional/mean (:price %))}))
-
-  ;; works
+  ;; year - works
   (-> raw-ds
       (adjust-interval :date [:symbol] :year)
       (tablecloth/aggregate {:price #(tech.v3.datatype.functional/mean (:price %))}))
 
+  ;; quarter - works
   (-> raw-ds
       (adjust-interval :date [:symbol] :quarter)
       (tablecloth/aggregate {:price #(tech.v3.datatype.functional/mean (:price %))}))
 
-  ;; ???
+  ;; quarter start - ???
   (-> raw-ds
       (adjust-interval :date [:symbol] :quarter-start))
 
+  ;; adjust to smaller interval - ??
+  (-> raw-ds
+      (adjust-interval :date [:symbol] :instant))
 
-  ;; LocalDate.now().get(IsoFields.QUARTER_OF_YEAR)
-  (.get #time/date "2020-10-10" java.time.temporal.IsoFields/QUARTER_OF_YEAR)
-
-  (-> (.get #time/date "2020-10-10" java.time.temporal.ChronoUnit/Quarter)
-      (type))
-
-  (YearQuarter/from #time/date "2020-10-10")
-
-)
+  ;; ungroup option - works
+  (-> raw-ds
+      (adjust-interval :date [:symbol] :quarter {:ungroup? true}))
+  )
