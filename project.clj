360b26946fb8c6d2650f806c3a34e16b06bfd95d
Ethan Miller <ethanzanemiller@gmail.com>
2021-03-09 22:23:34 -0800

    Add dev profile

--- project.clj
+++ project.clj
@@ -8,8 +8,8 @@
                  [tick "0.4.27-alpha"]
                  [scicloj/notespace "3-alpha3-SNAPSHOT"]
                  [aerial.hanami "0.12.4"]]
-  :plugins [[lein-cljfmt "0.7.0"]
-            [jonase/eastwood "0.3.14"]]
-  :aliases {"lint" ["do"
-                    ["cljfmt" "check"]
-                    ["eastwood" "{:source-paths [\"src\"]}"]]})
+  :profiles {:dev {:plugins [[lein-cljfmt "0.7.0"]
+                             [jonase/eastwood "0.3.14"]]
+                   :aliases {"lint" ["do"
+                                     ["cljfmt" "check"]
+                                     ["eastwood" "{:source-paths [\"src\"]}"]]}}})
