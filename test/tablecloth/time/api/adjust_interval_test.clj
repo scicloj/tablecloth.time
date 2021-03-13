44077a88ce2359b8b2871262b51f607cdcc94a51
Ethan Miller <ethanzanemiller@gmail.com>
2021-03-12 21:09:49 -0800

    Add another test (comment out midje tests)

--- test/tablecloth/time/api/adjust_interval_test.clj
+++ test/tablecloth/time/api/adjust_interval_test.clj
@@ -3,7 +3,8 @@
             [tablecloth.api :as tablecloth]
             [tablecloth.time.api.conversion :as converters]
             [tablecloth.time.api :refer [adjust-interval]]
-            [midje.sweet :refer [fact]]))
+            [midje.sweet :refer [fact contains]]
+            [clojure.test :refer [testing deftest is]]))
 
 (defn time-series [start-inst n tf]
   (plus-temporal-amount start-inst (range n) tf))
@@ -13,9 +14,23 @@
                        :symbol "MSFT"
                        :price (take num-rows (repeatedly #(rand 200)))}))
 
-(fact "returns a grouped dataset"
-      (-> (get-test-ds #time/instant "1970-01-01T00:00:00.000Z"
-                       (* 2 60)
-                       :seconds)
-          (adjust-interval :idx [:symbol] converters/->minutes :minutes)
-          (tablecloth/grouped?)) => true)
+;; (fact "`adjust-interval` returns a grouped dataset"
+;;       (let [result (-> (get-test-ds #time/instant "1970-01-01T00:00:00.000Z" (* 2 60) :seconds)
+;;                        (adjust-interval :idx [:symbol] converters/->minutes :minutes))]
+;;         (tablecloth/grouped? result) => false))
+
+;; (fact "it passes through column specified as `keys`"
+;;       (let [result (-> (get-test-ds #time/instant "1970-01-01T00:00:00.000Z" (* 2 60) :seconds)
+;;                        (adjust-interval :idx [:symbol] converters/->minutes :minutes))]
+;;         (tablecloth/column-names result) => (contains [:symbol])))
+
+(deftest test-adjust-internval
+  (testing "it returns a grouped dataset"
+    (is (-> (get-test-ds #time/instant "1970-01-01T00:00:00.000Z" (* 2 60) :seconds)
+            (adjust-interval :idx [:symbol] converters/->minutes :minutes)
+            (tablecloth/grouped?))))
+
+  (testing "it passes through columns specified as `keys`"
+    (let [result (-> (get-test-ds #time/instant "1970-01-01T00:00:00.000Z" (* 2 60) :seconds)
+                     (adjust-interval :idx [:symbol] converters/->minutes :minutes))]
+      (is (some #{:symbol} (tablecloth/column-names result))))))
