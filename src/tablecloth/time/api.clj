7911200137328bd85f1a5be336bf480579e30e89
Ethan Miller <ethanzanemiller@gmail.com>
2021-03-13 18:26:02 -0800

    Clean up

--- src/tablecloth/time/api.clj
+++ src/tablecloth/time/api.clj
@@ -1,5 +1,4 @@
 (ns tablecloth.time.api
-  ;; {:clj-kondo/config '{:linters {:unresolved-symbol {:level :off}}}}
   (:require [tech.v3.datatype.export-symbols :as exporter]))
 
 (exporter/export-symbols tablecloth.time.api.slice
