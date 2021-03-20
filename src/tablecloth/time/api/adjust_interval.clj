30f3abd9817971f226888a5a02d4134ae4ff5b40
Ethan Miller <ethanzanemiller@gmail.com>
2021-03-19 18:03:48 -0700

    Change arg name

--- src/tablecloth/time/api/adjust_interval.clj
+++ src/tablecloth/time/api/adjust_interval.clj
@@ -7,9 +7,9 @@
   whose values are the result of applying `time-converter` to the column specified
   by `index-column-name`, and then performing a `group-by` operation on that column
   and any columns specified by `keys`."
-  [dataset index-column-name keys time-converter new-column-name]
+  [dataset index-column-name keys ->new-time-converter new-column-name]
   (let [index-column (index-column-name dataset)
-        target-datatype (-> index-column first time-converter elemwise-datatype)
+        target-datatype (-> index-column first ->new-time-converter elemwise-datatype)
         adjusted-column-data (emap time-converter target-datatype index-column)]
     (-> dataset
         (tablecloth/add-or-replace-column new-column-name adjusted-column-data)
