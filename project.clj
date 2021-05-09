02be926cff121159e6b1b3f700e6eef761d54149
Ethan Miller <ethanzanemiller@gmail.com>
2021-05-08 21:54:48 -0700

    Only resolve :dev aliases deps.edn for :dev profile

--- project.clj
+++ project.clj
@@ -7,13 +7,14 @@
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
 
-  :lein-tools-deps/config {:config-files [:install :user :project]
-                           :aliases [:dev]}
+  :lein-tools-deps/config {:config-files [:install :user :project]}
 
-  :profiles {:dev {:aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]
+  :profiles {:dev {:lein-tools-deps/config {:aliases [:dev]}
+                   :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]
                              "lint" ["do"
                                      ["cljfmt" "check"]
                                      ["run" "-m" "clj-kondo.main" "--lint" "src:test"]]}
                    :plugins [[lein-midje "3.2.1"]
-                             [lein-cljfmt "0.7.0"]]}})
+                             [lein-cljfmt "0.7.0"]]}}
+  )
 
