c8f28aa272174d15127772aa2498bff224695184
Ethan Miller <ethanzanemiller@gmail.com>
2021-05-18 23:04:41 -0700

    Enable time-literals in api namespace

--- src/tablecloth/time/api.clj
+++ src/tablecloth/time/api.clj
@@ -1,6 +1,9 @@
 (ns tablecloth.time.api
   {:clj-kondo/config '{:linters {:unresolved-symbol {:level :off}}}}
-  (:require [tech.v3.datatype.export-symbols :as exporter]))
+  (:require [tech.v3.datatype.export-symbols :as exporter]
+            [tablecloth.time.time-literals :refer [modify-printing-of-time-literals-if-enabled!]]))
+
+(modify-printing-of-time-literals-if-enabled!)
 
 (exporter/export-symbols tablecloth.time.api.slice
                          slice)
