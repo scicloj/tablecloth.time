10d33e15b6c06c1e5a2a6d3efae3d6c49a826499
Ethan Miller <ethanzanemiller@gmail.com>
2021-05-22 11:26:11 -0700

    Fix docstring

--- src/tablecloth/time/utils/datatypes.clj
+++ src/tablecloth/time/utils/datatypes.clj
@@ -2,11 +2,12 @@
   (:require [tech.v3.datatype :refer [elemwise-datatype]]
             [clojure.set :refer [union]]))
 
-(defn get-datatype [data]
+(defn get-datatype 
   "Returns the datatype keyword of `data` as identified by dtype-next's
-type system. If it is not a known type, it will return `:object`. `data`
-can be a column (or some type of 'reader' as defined by dtype-next) or
-an individual element of data."
+  type system. If it is not a known type, it will return `:object`. `data`
+  can be a column (or some type of 'reader' as defined by dtype-next) or
+  an individual element of data."
+  [data]
   (elemwise-datatype data))
 
 (def time-datatypes
