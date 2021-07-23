841b8f87b44f053313b651d89a615197c08e4dce
Ethan Miller <ethanzanemiller@gmail.com>
2021-07-23 11:27:37 -0700

    Update docstring

--- src/tablecloth/time/api/adjust_interval.clj
+++ src/tablecloth/time/api/adjust_interval.clj
@@ -18,7 +18,32 @@
   "Adjusts the interval of the time index column by applying the
   `converter` function to the values in the time index. Returns a
   grouped dataset that can be used with `tablecloth.api.aggregate`,
-  for example."
+  for example.
+
+  
+  Options are:
+
+  - also-group-by - include other columns in the grouping if needed
+
+    Example Data:
+  
+    |         :A |  :B | :C |
+    |------------|-----|---:|
+    | 1970-01-01 | foo |  0 |
+    | 1970-01-02 | bar |  1 |
+    | 1970-02-01 | foo |  2 |
+    | 1970-02-02 | bar |  3 |
+
+    (-> data
+        (index-by :A)
+        (adjust-interval ->months-end {:also-group-by [:B]}))
+    ;; => _unnamed [3 3]:
+    ;;    | :summary |  :B |         :A |
+    ;;    |---------:|-----|------------|
+    ;;    |      0.5 | foo | 1970-01-31 |
+    ;;    |      2.0 | foo | 1970-02-28 |
+    ;;    |      3.0 | bar | 1970-02-28 |
+  "
   ([dataset converter]
    (adjust-interval dataset converter nil))
   ([dataset converter {:keys [also-group-by]}]
