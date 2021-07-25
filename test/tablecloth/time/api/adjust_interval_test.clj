e2010f7c0d16561d3fe0162db8c723537f7a0663
Ethan Miller <ethanzanemiller@gmail.com>
2021-07-24 19:35:09 -0700

    Make tests work

--- test/tablecloth/time/api/adjust_interval_test.clj
+++ test/tablecloth/time/api/adjust_interval_test.clj
@@ -1,7 +1,7 @@
 (ns tablecloth.time.api.adjust-interval-test
   (:require [tech.v3.datatype.datetime :refer [plus-temporal-amount]]
             [tablecloth.api :refer [dataset grouped? column-names]]
-            [tablecloth.time.api.converters :as converters]
+            [tablecloth.time.api.converters :refer [->minutes]]
             [tablecloth.time.api :refer [adjust-interval]]
             [clojure.test :refer [testing deftest is]]))
 
@@ -10,24 +10,27 @@
 
 (defn get-test-ds [start-time num-rows temporal-unit]
   (dataset {:idx (time-index start-time num-rows temporal-unit)
-            :key-a "FOO"
-            :key-b "BAR"
+            :key-a (reduce into [] (take (/ num-rows 2) (repeatedly #(vector "FOO" "BAR"))))
+            :key-b (reduce into [] (take (/ num-rows 2) (repeatedly #(vector "FOO" "BAR"))))
             :value (take num-rows (repeatedly #(rand 200)))}))
 
-(deftest test-adjust-internval
-  (let [test-dataset (get-test-ds #time/instant "1970-01-01T00:00:00.000Z"
-                                  (* 2 60)
-                                  :seconds)
-        result (-> test-dataset (adjust-interval :idx
-                                                 [:key-a, :key-b]
-                                                 converters/->minutes
-                                                 :minutes))]
+(deftest test-adjust-interval
+  (let [ds (get-test-ds #time/instant "1970-01-01T00:00:00.000Z"
+                        (* 2 60)
+                        :seconds)]
 
     (testing "it returns a grouped dataset"
-      (is (-> result (grouped?))))
+      (let [result (-> ds (adjust-interval ->minutes))]
+       (is (-> result (grouped?)))))
 
-    (testing "it passes through columns specified as `keys`"
-      (is (some #{:key-a :key-b} (column-names result))))
+    (testing ":also-group-by option"
+      (let [result (-> ds (adjust-interval ->minutes
+                                           {:also-group-by [:key-a :key-b]}))]
+        (is (some #{:key-a :key-b} (column-names result)))))
 
-    (testing "it creates a new time column specified by `new-column-name`"
-      (is (some #{:minutes} (column-names result))))))
+    ;; No support for this now b/c we aren't doing bookeeping on the :index meta data yet
+    ;; (testing ":rename-index-to option"
+    ;;   (let [result (-> ds (adjust-interval ->minutes
+    ;;                                        {:rename-index-to :minutes}))]
+    ;;     (is (some #{:minutes} (column-names result)))))
+    ))
