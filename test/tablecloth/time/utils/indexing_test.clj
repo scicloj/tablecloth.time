12b4f1808ae6b3dd2109d390c944ce6631b20872
Ethan Miller <ethanzanemiller@gmail.com>
2021-07-30 15:52:36 -0700

    Rearrange tests

--- test/tablecloth/time/utils/indexing_test.clj
+++ test/tablecloth/time/utils/indexing_test.clj
@@ -15,10 +15,10 @@
       (is (= nil
              (-> ds
                  (index-by :x)
-                 (rename-columns {:x :z})
+                 (rename-columns {:x :z}) ;; invalidates index
                  (idx-utils/index-column-name))))))
 
-  (testing "column auto detection by time type"
+  (testing "when there is only one time column"
     (let [ds (dataset {:x [#time/date "1970-01-01"
                            #time/date "1970-01-02"
                            #time/date "1970-01-03"]
@@ -28,11 +28,17 @@
       (is (= :z
              (-> ds
                  (rename-columns {:x :z})
-                 (idx-utils/index-column-name))))
+                 (idx-utils/index-column-name))))))
+
+  (testing "when more than one time column and no index metadata"
+    (let [ds (dataset {:x [#time/date "1970-01-01"
+                           #time/date "1970-01-02"
+                           #time/date "1970-01-03"]
+                       :y [4 5 6]})]
       (is (= nil
+
              (-> ds
                  (add-column :z [#time/date "1970-01-01"
                                  #time/date "1970-01-02"
                                  #time/date "1970-01-03"])
                  (idx-utils/index-column-name)))))))
-
