a9aa5a8097df48fea1e0c957fbbd596770ca028d
Ethan Miller <ethanzanemiller@gmail.com>
2021-07-30 11:53:18 -0700

    Add index-by test that it uses validatable

---
+++ test/tablecloth/time/api/index_by_test.clj
@@ -0,0 +1,16 @@
+(ns tablecloth.time.api.index-by-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [tablecloth.time.api.index_by :refer [index-by]]
+            [tablecloth.api :refer [dataset]]))
+
+(deftest index-by-test
+  (testing "index 'validatable'"
+    (let [ds-with-index (-> (dataset {:x [1 2 3]
+                                      :y [4 5 6]})
+                            (index-by :x))]
+      (is (contains? (meta ds-with-index)
+                     :validatable))
+      (is (contains? (:validatable (meta ds-with-index))
+                     :index)))))
+
+
