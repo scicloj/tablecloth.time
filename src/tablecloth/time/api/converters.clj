75172a3cba12d46caabb4ecaecc95cd48869e962
Ethan Miller <ethanzanemiller@gmail.com>
2021-11-18 09:27:31 -0800

    Add some extractors

--- src/tablecloth/time/api/converters.clj
+++ src/tablecloth/time/api/converters.clj
@@ -1,5 +1,5 @@
 (ns tablecloth.time.api.converters
-  (:import [java.time Year]
+  (:import [java.time Year Month LocalDateTime]
            [org.threeten.extra YearWeek YearQuarter])
   (:require [tech.v3.datatype.datetime :as dtdt]
             [tech.v3.datatype :as dt]
@@ -175,3 +175,65 @@
   (let [^java.time.LocalDate localDate (-> datetime ->local-date)]
     (.with localDate (java.time.temporal.TemporalAdjusters/lastDayOfYear))))
 
+(defn- extract-datetime-component-simple
+  [extract-fn datetime]
+  (-> datetime
+      (->local-date-time)
+      (extract-fn)))
+
+;; Would be nice to type hint `time-component`. May need a macro for this.
+(defn- extract-datetime-component-complex
+  ([extract-fn datetime]
+   (extract-datetime-component-complex
+    extract-fn
+    datetime
+    {:as-number? false :as-class? false}))
+  ([extract-fn datetime {:keys [as-number? as-class?]}]
+   (let [^LocalDateTime ldt (->local-date-time datetime)
+         time-component (extract-fn ldt)]
+     (cond
+       as-number? (.getValue time-component) ;; reflection warning
+       as-class? time-component
+       :else (.toString ^java.lang.Object time-component)))))
+
+(def ^{:doc "Extracts year (as number) from any datetime."
+       :argslist '([datetime])}
+  year (partial extract-datetime-component-simple
+                #(.getYear ^LocalDateTime %)))
+
+(def ^{:doc "Extracts year (as number) from any datetime."
+       :argslist '([datetime])}
+  dayofyear (partial extract-datetime-component-simple
+                     #(.getYear ^LocalDateTime %)))
+
+(def ^{:doc "Extract month from any datetime."
+       :argslist '([datetime] [datetime {:keys [as-number? as-class?]}])}
+  month (partial
+         extract-datetime-component-complex
+         #(.getMonth ^LocalDateTime %)))
+
+(def ^{:doc "Extracts the day of the month from any datetime."
+       :argslist '([datetime])}
+  dayofmonth (partial extract-datetime-component-simple
+                      #(.getDayOfMonth ^LocalDateTime %)))
+
+(def ^{:doc "Extract the day of week from any datetime."
+       :argslist '([datetime] [datetime {:keys [as-number? as-class?]}])}
+  dayofweek (partial
+             extract-datetime-component-complex
+             #(.getDayOfWeek ^LocalDateTime %)))
+
+(def ^{:doc "Extracts the hour from any datetime." 
+       :arglists '([datetime])}
+  hour (partial extract-datetime-component-simple
+                  #(.getHour ^LocalDateTime %)))
+
+(def ^{:doc "Extracts the minute of hour (number) from any datetime."
+       :arglists '([datetime])}
+  minute (partial extract-datetime-component-simple
+                  #(.getMinute ^LocalDateTime %)))
+
+(def ^{:doc "Extracts the second of minute from any datetime."
+       :arglists '([datetime])}
+  seconds (partial extract-datetime-component-simple
+                  #(.getSecond ^LocalDateTime %)))
