2c4b8ddbd7f1f6db53388f09ce285b0876785765
Ethan Miller <ethanzanemiller@gmail.com>
2025-12-27 22:19:43 +0200

    Added floor method for :years, :months, :quarters

--- src/tablecloth/time/column/api.clj
+++ src/tablecloth/time/column/api.clj
@@ -1,12 +1,12 @@
 (ns tablecloth.time.column.api
   (:require [tech.v3.datatype :as dtype]
             [tech.v3.datatype.functional :as fun]
-            [tech.v3.datatype.packing :as dt-packing]
             [tech.v3.datatype.casting :as casting]
+            [tech.v3.datatype.packing :as dt-packing]
             [tech.v3.datatype.datetime :as dtdt]
             [tech.v3.datatype.datetime.base :as dtdt-base]
-            [tablecloth.column.api :as tcc]
-            [tablecloth.time.api.parse :refer [parse]])
+            [tech.v3.datatype.datetime.operations :as dtdt-ops]
+            [tablecloth.column.api :as tcc])
   (:import [java.time ZoneId Instant ZonedDateTime LocalDate LocalDateTime
             Duration LocalTime]))
 
@@ -19,19 +19,12 @@
 
 (def ^:private targets
   (apply conj dtdt-base/datatypes
-              dtdt-base/epoch-datatypes))
+         dtdt-base/epoch-datatypes))
 
 (def ^:private synonyms
   {:zdt :zoned-date-time
    :odt :offset-date-time
    :ldt :local-date-time})
-
-(defn metric-unit? [u]
-  (boolean (#{:milliseconds :seconds :minutes :hours :days :weeks}
-            (normalize-unit u))))
-
-;; (defn calendar-unit? [u]
-;;   (boolean (#{:months :quarters :years} (normalize-unit u))))
 
 (defn calendar-local-type? [dtype]
   (let [calendar-local-types #{:local-date :local-date-time :local-time}
@@ -65,18 +58,18 @@
   (^ZoneId [z]
    (coerce-zone-id z {:default (dtdt/system-zone-id)}))
   ([z opts]
-    (cond
-      (nil? z) (:default opts)
-        (instance? ZoneId z) ^ZoneId z
+   (cond
+     (nil? z) (:default opts)
+     (instance? ZoneId z) ^ZoneId z
 
-        (string? z) (ZoneId/of ^String z)
-        :else (throw (ex-info (str "Unsupported zone value: " (pr-str z))
-                              {:type ::unsupported-zone
-                              :value z})))))
+     (string? z) (ZoneId/of ^String z)
+     :else (throw (ex-info (str "Unsupported zone value: " (pr-str z))
+                           {:type ::unsupported-zone
+                            :value z})))))
 
 (defn ^:private coerce-column
   "Coerce to a column."
-  [data opts]
+  [data]
   (if (tcc/column? data)
     data
     (tcc/column data)))
@@ -90,11 +83,22 @@
     :minute  :minutes
     :hour    :hours
     :day     :days
+    :week    :weeks
+    :month   :months
     :year    :years
-    ;; :week    :weeks
-    ;; :month   :months
-    ;; :quarter :quarters
+    :quarter :quarters
     u))
+
+(defn ^:private metric-unit?
+  "Check if unit is a metric/fixed-duration unit."
+  [u]
+  (boolean (#{:milliseconds :seconds :minutes :hours :days :weeks}
+            (normalize-unit u))))
+
+(defn ^:private calendar-unit?
+  "Check if unit is a calendar/variable-duration unit."
+  [u]
+  (boolean (#{:months :quarters :years} (normalize-unit u))))
 
 (defn convert-time
   "Convert a time column between temporal and epoch representations.
@@ -135,7 +139,7 @@
              col
              (let [src-factor (dtdt-base/epoch->microseconds src-type)
                    tgt-factor (dtdt-base/epoch->microseconds tgt-type)
-                   conversion (fun// src-factor tgt-factor )]
+                   conversion (fun// src-factor tgt-factor)]
                (fun/* col conversion)))
            (throw
             (ex-info
@@ -152,7 +156,6 @@
               :tgt-category  tgt-cat})))]
      (tcc/column data))))
 
-
 (defn milliseconds-in
   "Return the number of epoch millis in a unit.
 
@@ -160,61 +163,94 @@
   [unit]
   (case unit
     :milliseconds 1
-    :seconds      dtdt/milliseconds-in-second 
-    :minutes      dtdt/milliseconds-in-minute 
+    :seconds      dtdt/milliseconds-in-second
+    :minutes      dtdt/milliseconds-in-minute
     :hours        dtdt/milliseconds-in-hour
     :days         dtdt/milliseconds-in-day
     :weeks        dtdt/milliseconds-in-week
     (throw (ex-info (str "Unsupported unit: " unit) {:unit unit}))))
 
+(defn- local-date->epoch-month [col-ld]
+  (let [col-year (dtdt-ops/long-temporal-field :years col-ld)
+        col-year-month (dtdt-ops/long-temporal-field :months col-ld)
+        col-epoch-month (fun/+ (fun/* 12 (fun/- col-year 1970))
+                               (fun/- col-year-month 1))]
+    col-epoch-month))
+
+(defn- local-date->epoch-year [col-ld]
+  (let [col-year (dtdt-ops/long-temporal-field :years col-ld)
+        col-epoch-year (fun/- col-year 1970)]
+    col-epoch-year))
+
+(defn floor-to-year
+  "Floor temporal column values to the closest year interval."
+  ([col interval]
+   (floor-to-year col interval {:zone (dtdt/system-zone-id)}))
+  ([col interval opts]
+   (let [col (coerce-column col)
+         original-type (dt-packing/unpack-datatype (dtype/elemwise-datatype col))
+         col-ld (convert-time col :local-date opts)
+         col-epoch-year (local-date->epoch-year col-ld)
+         col-floored (fun/- col-epoch-year (fun/rem col-epoch-year interval))
+         result-ld (dtdt-ops/plus-temporal-amount (LocalDate/of 1970 1 1) col-floored :years)]
+     (convert-time result-ld original-type opts))))
+
+(defn floor-to-month
+  "Floor a temporal columns values the closest month interval."
+  ([col interval]
+   (floor-to-month col interval {:zone (dtdt/system-zone-id)}))
+  ([col interval opts]
+   (let [col (coerce-column col)
+         original-type (dt-packing/unpack-datatype (dtype/elemwise-datatype col))
+         col-ld (convert-time col :local-date opts)
+         col-epoch-month (local-date->epoch-month col-ld)
+         col-floored (fun/- col-epoch-month (fun/rem col-epoch-month interval))
+         result-ld (dtdt-ops/plus-temporal-amount (LocalDate/of 1970 1 1) col-floored :months)]
+     (convert-time result-ld original-type opts))))
+
+(defn- local-date->epoch-quarter
+  "Convert LocalDate column to epoch-quarters (quarters since 1970-Q1)."
+  [col-ld]
+  (let [col-epoch-month (local-date->epoch-month col-ld)
+        col-epoch-quarter (fun/quot col-epoch-month 3)]
+    col-epoch-quarter))
+
+(defn floor-to-quarter
+  "Floor temporal column values to the closest quarter interval."
+  ([col interval]
+   (floor-to-quarter col interval {:zone (dtdt/system-zone-id)}))
+  ([col interval opts]
+   (let [col (coerce-column col)
+         original-type (dt-packing/unpack-datatype (dtype/elemwise-datatype col))
+         col-ld (convert-time col :local-date opts)
+         col-epoch-quarter (local-date->epoch-quarter col-ld)
+         col-floored (fun/- col-epoch-quarter (fun/rem col-epoch-quarter interval))
+         ;; Convert quarters back to months, then add to epoch
+         col-floored-months (fun/* col-floored 3)
+         result-ld (dtdt-ops/plus-temporal-amount (LocalDate/of 1970 1 1) col-floored-months :months)]
+     (convert-time result-ld original-type opts))))
+
 (defn down-to-nearest
   ""
   ([col interval unit opts]
-   (let [unit (normalize-unit unit)
+   (let [col (coerce-column col)
+         original-type (dt-packing/unpack-datatype (dtype/elemwise-datatype col))
+         unit (normalize-unit unit)
          zone (coerce-zone-id (:zone opts))]
      (cond
        (metric-unit? unit)
-       (let [original-type (dt-packing/unpack-datatype (dtype/elemwise-datatype col))
-             divisor (* (long interval) (long (milliseconds-in unit)))
+       (let [divisor (* (long interval) (long (milliseconds-in unit)))
              millis-col (convert-time col :epoch-milliseconds {:zone zone})
-             rounded-col (dtype/set-datatype 
+             rounded-col (dtype/set-datatype
                           ;; must set type b/c arithmetic comes back
                           ;; as :int64 -- the storage type of
                           ;; epoch-milliseconds
                           (fun/- millis-col (fun/rem millis-col divisor))
                           :epoch-milliseconds)]
-         (convert-time rounded-col original-type (:zone zone)))))))
+         (convert-time rounded-col original-type (:zone zone)))
+       (calendar-unit? unit)
+       (let [column-zdt (convert-time col :zoned-date-time {:zone zone})
+             rounded-col (case unit
+                           :months (floor-to-month interval opts))]
+         (convert-time rounded-col original-type))))))
 
-(comment
-  ;; source days => target hours
-
-  ;; milliseconds -> days 
-
-  (tcc/column (tcc/column))
-
-  (fun//
-   (dt-base/epoch->microseconds :epoch-milliseconds)
-   (dt-base/epoch->microseconds :epoch-days))
-
-  (dt-base/epoch->microseconds :epoch-hours)
-
-  (parse "2025-12-10")
-
-  (def ddata (tcc/column
-              (map parse ["1970-01-02T13:33:00Z" "1970-01-10T03:10:10Z"])))
-
-  (-> ddata
-      (convert-time :epoch-milliseconds))
-
-  (-> ddata
-      (convert-time :epoch-milliseconds)
-      (convert-time :epoch-days))
-
-  (tcc/typeof ddata)
-
-  (dtdt/datetime-datatype? (dtype/elemwise-datatype ddata))
-
-  (-> ddata
-      (down-to-nearest 12 :minutes (:zone "America/Chicago")))
-
-  )
--- test/tablecloth/time/column/api_test.clj
+++ test/tablecloth/time/column/api_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer [deftest testing is]]
             [tablecloth.column.api :as tcc]
             [tablecloth.time.api.parse :refer [parse]]
-            [tablecloth.time.column.api :refer [convert-time]])
+            [tablecloth.time.column.api :refer [convert-time floor-to-month floor-to-quarter floor-to-year]])
   (:import [java.time Duration Instant LocalDate LocalDateTime ZonedDateTime]))
 
 (deftest convert-time-temporal->epoch-default-utc
@@ -77,3 +77,152 @@
             false
             (catch clojure.lang.ExceptionInfo _
               true))))))
+
+(deftest floor-to-month-test
+  (testing "floor to 1-month intervals"
+    (let [dates [(parse "1970-01-15")
+                 (parse "1970-03-20")
+                 (parse "1970-06-10")]
+          col (tcc/column dates)
+          result (floor-to-month col 1)]
+      (is (= [(parse "1970-01-01")
+              (parse "1970-03-01")
+              (parse "1970-06-01")]
+             (vec result)))))
+
+  (testing "floor to 6-month intervals"
+    (let [dates [(parse "1970-01-15")  ; month 0 -> bucket 0
+                 (parse "1970-06-20")  ; month 5 -> bucket 0
+                 (parse "1970-07-10")  ; month 6 -> bucket 1
+                 (parse "1970-12-31")] ; month 11 -> bucket 1
+          col (tcc/column dates)
+          result (floor-to-month col 6)]
+      (is (= [(parse "1970-01-01")
+              (parse "1970-01-01")
+              (parse "1970-07-01")
+              (parse "1970-07-01")]
+             (vec result)))))
+
+  (testing "floor to 3-month intervals"
+    (let [dates [(parse "1970-01-15")  ; month 0 -> bucket 0
+                 (parse "1970-03-20")  ; month 2 -> bucket 0
+                 (parse "1970-04-10")  ; month 3 -> bucket 1
+                 (parse "1970-07-15")] ; month 6 -> bucket 2
+          col (tcc/column dates)
+          result (floor-to-month col 3)]
+      (is (= [(parse "1970-01-01")
+              (parse "1970-01-01")
+              (parse "1970-04-01")
+              (parse "1970-07-01")]
+             (vec result)))))
+
+  (testing "preserves column type - Instant"
+    (let [instants [(Instant/parse "1970-03-15T12:30:45Z")
+                    (Instant/parse "1970-06-20T08:15:30Z")]
+          col (tcc/column instants)
+          result (floor-to-month col 1 {:zone "UTC"})]
+      (is (every? #(instance? Instant %) result))
+      (is (= [(Instant/parse "1970-03-01T00:00:00Z")
+              (Instant/parse "1970-06-01T00:00:00Z")]
+             (vec result))))))
+
+(deftest floor-to-quarter-test
+  (testing "floor to 1-quarter intervals"
+    (let [dates [(parse "1970-01-15")  ; Q1
+                 (parse "1970-04-20")  ; Q2
+                 (parse "1970-07-10")  ; Q3
+                 (parse "1970-10-25")] ; Q4
+          col (tcc/column dates)
+          result (floor-to-quarter col 1)]
+      (is (= [(parse "1970-01-01")
+              (parse "1970-04-01")
+              (parse "1970-07-01")
+              (parse "1970-10-01")]
+             (vec result)))))
+
+  (testing "floor to 2-quarter intervals (half-years)"
+    (let [dates [(parse "1970-01-15")  ; Q1 (quarter 0) -> bucket 0
+                 (parse "1970-06-20")  ; Q2 (quarter 1) -> bucket 0
+                 (parse "1970-07-10")  ; Q3 (quarter 2) -> bucket 1
+                 (parse "1970-12-31")] ; Q4 (quarter 3) -> bucket 1
+          col (tcc/column dates)
+          result (floor-to-quarter col 2)]
+      (is (= [(parse "1970-01-01")
+              (parse "1970-01-01")
+              (parse "1970-07-01")
+              (parse "1970-07-01")]
+             (vec result)))))
+
+  (testing "floor to 4-quarter intervals (years)"
+    (let [dates [(parse "1970-01-15")  ; Q0 -> bucket 0 (Q0-3)
+                 (parse "1970-12-31")  ; Q3 -> bucket 0 (Q0-3)
+                 (parse "1971-03-15")  ; Q4 -> bucket 1 (Q4-7)
+                 (parse "1971-12-31")] ; Q7 -> bucket 1 (Q4-7)
+          col (tcc/column dates)
+          result (floor-to-quarter col 4)]
+      (is (= [(parse "1970-01-01")
+              (parse "1970-01-01")
+              (parse "1971-01-01")  ; Q4 floors to Q4 = 1971-01-01
+              (parse "1971-01-01")]
+             (vec result)))))
+
+  (testing "quarters spanning multiple years"
+    (let [dates [(parse "1971-03-15")  ; Q4 (1971-Q1)
+                 (parse "1972-06-20")] ; Q9 (1972-Q2)
+          col (tcc/column dates)
+          result (floor-to-quarter col 3)]
+      ;; Q0=1970-Jan-Mar, Q1=Apr-Jun, Q2=Jul-Sep, Q3=Oct-Dec
+      ;; Q4=1971-Jan-Mar, Q5=Apr-Jun, Q6=Jul-Sep, Q7=Oct-Dec
+      ;; Q8=1972-Jan-Mar, Q9=Apr-Jun
+      ;; With interval 3: buckets are Q0-2, Q3-5, Q6-8, Q9-11
+      ;; 1971-Q1 = Q4 -> Q4 - (Q4 % 3) = 4 - 1 = Q3 = 1970-10-01
+      ;; 1972-Q2 = Q9 -> Q9 - (Q9 % 3) = 9 - 0 = Q9 = 1972-04-01
+      (is (= [(parse "1970-10-01")
+              (parse "1972-04-01")]
+             (vec result))))))
+
+(deftest floor-to-year-test
+  (testing "floor to 1-year intervals"
+    (let [dates [(parse "1970-06-15")
+                 (parse "1971-03-20")
+                 (parse "1972-12-25")]
+          col (tcc/column dates)
+          result (floor-to-year col 1)]
+      (is (= [(parse "1970-01-01")
+              (parse "1971-01-01")
+              (parse "1972-01-01")]
+             (vec result)))))
+
+  (testing "floor to 5-year intervals"
+    (let [dates [(parse "1970-06-15")  ; year 0 -> bucket 0
+                 (parse "1974-03-20")  ; year 4 -> bucket 0
+                 (parse "1975-01-01")  ; year 5 -> bucket 1
+                 (parse "1979-12-31")  ; year 9 -> bucket 1
+                 (parse "1980-06-15")] ; year 10 -> bucket 2
+          col (tcc/column dates)
+          result (floor-to-year col 5)]
+      (is (= [(parse "1970-01-01")
+              (parse "1970-01-01")
+              (parse "1975-01-01")
+              (parse "1975-01-01")
+              (parse "1980-01-01")]
+             (vec result)))))
+
+  (testing "floor to 10-year intervals (decades)"
+    (let [dates [(parse "1975-06-15")
+                 (parse "1989-12-31")
+                 (parse "1990-01-01")
+                 (parse "2005-06-15")]
+          col (tcc/column dates)
+          result (floor-to-year col 10)]
+      (is (= [(parse "1970-01-01")
+              (parse "1980-01-01")
+              (parse "1990-01-01")
+              (parse "2000-01-01")]
+             (vec result)))))
+
+  (testing "preserves column type - LocalDate"
+    (let [dates [(LocalDate/of 1972 6 15)]
+          col (tcc/column dates)
+          result (floor-to-year col 1)]
+      (is (every? #(instance? LocalDate %) result)))))
