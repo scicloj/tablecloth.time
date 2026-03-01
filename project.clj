30998f7b9a87455f4a8c721f3fe6566549d7d64e
Ethan Miller <ethanzanemiller@gmail.com>
2026-02-28 19:55:46 -0500

    Bump version to 1.00-alpha-7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.scicloj/tablecloth.time "1.00-alpha-6"
+(defproject org.scicloj/tablecloth.time "1.00-alpha-7-SNAPSHOT"
   :description "A time series manipulation library built on top of tablecloth."
   :url "https://github.com/scicloj/tablecloth.time"
   :license {:name "The MIT Licence"
